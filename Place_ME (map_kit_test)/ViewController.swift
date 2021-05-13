22f4d4a8f66dacd0b112fbb65f93ca17b060d121
Dmitry Ihnatiev <straenger@gmail.com>
2021-05-13 18:19:41 +0300

    Initial Commit

---
+++ Place_ME (map_kit_test)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Place_ME (map_kit_test)
+//
+//  Created by Дмитрий Игнатьев on 13.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
